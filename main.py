c4eff8f0459b50bbd9be488d1a81fdfd3e0fe967
Petrov <georgi.petrov@cognyte.com>
2021-10-27 12:56:39 +0300

    Add parser for the main Sofia District site

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ def load_sites_dict():
 
 def main():
     sites_dict = load_sites_dict()
-    parser = parsers_dict['Sredec']
-    response = requests.get(sites_dict['Sredec']['talks_url'])
-    response.encoding = 'utf-8' 
-    html_str = response.text
-    feed_list = parser(html_str)
-    print(feed_list)
+    TALKS_URL = 'talks_url'
+    
+    for region in sites_dict:
+        # If the region name has changed or if we haven't written a parser yet
+        # we cannot handle this region
+        if region not in parsers_dict or not parsers_dict[region] or TALKS_URL not in sites_dict[region]:
+            continue 
+        
+        parser = parsers_dict[region]
+
+        response = requests.get(sites_dict[region][TALKS_URL])
+        response.encoding = 'utf-8' 
+        html_str = response.text
+        feed_list = parser(html_str)
+        print(feed_list)
 
 if __name__ == "__main__":
     main()
--- parsers.py
+++ parsers.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def sredec_parser(html_str):
     soup = BeautifulSoup(html_str, 'html.parser')
@@ -10,6 +11,51 @@ def sredec_parser(html_str):
         a_elem = item.find("a")
         link = a_elem['href']
         title = a_elem['title'].strip()
+        #TODO: refactor this as a dataclass?
+        result_list.append({
+            "Date": date,
+            "Title": title,
+            "Link": link
+        })
+
+    return result_list
+
+def krasno_selo_parser(html_str):
+    soup = BeautifulSoup(html_str, 'html.parser')
+    items = soup.select("#right > div.cont > div.ctxt > div.fnt_siz > p")
+    result_list = []
+    for item in items:
+        try:
+            a_elem = item.find("a")
+            if item.text.isspace() or not a_elem:
+                continue
+            date = ""
+            title = ""
+            link = item.find("a")['href']
+        except Exception as ex:
+            print(item)
+            exit()
+        #TODO: refactor this as a dataclass?
+        result_list.append({
+            "Date": date,
+            "Title": title,
+            "Link": link
+        })
+
+    return result_list
+
+
+def sofia_parser(html_str):
+    soup = BeautifulSoup(html_str, 'html.parser')
+    items = soup.select("#p_p_id_101_INSTANCE_f7DDrCt6UHSA_ > div > div > table > tbody > tr")
+    result_list = []
+    for item in items:
+        date = datetime.strptime(item.select("td")[1].text.strip(), "%d.%m.%Y")
+        a_elem = item.find("a")
+        title = a_elem.text
+        link = a_elem['href']
+
+        #TODO: refactor this as a dataclass?
         result_list.append({
             "Date": date,
             "Title": title,
@@ -19,7 +65,8 @@ def sredec_parser(html_str):
     return result_list
 
 parsers_dict = {
-    "Sredec": sredec_parser,
+    # "Sredec": sredec_parser,
+    "Sofia": sofia_parser,
     "KrasnoSelo": None,
     "Vazrajdane": None,
     "Oborishte": None,
